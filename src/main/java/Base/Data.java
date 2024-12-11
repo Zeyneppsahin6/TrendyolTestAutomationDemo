abdb928ca781bbbcdc81a9f4ec8b7187d3285998
Zeyneppsahin6 <zeynepsahin022@gmail.com>
2024-12-11 03:31:36 +0300

    Page Object Model yapısı oluşturulmuştur.

---
+++ src/main/java/Base/Data.java
@@ -0,0 +1,8 @@
+package Base;
+
+public class Data {
+
+    public String mail = "zeynepsahin022@icloud.com";
+    public String password = "Abcdef123!";
+    public String URL = "https://www.trendyol.com/giris?cb=%2F";
+}
