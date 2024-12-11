3c2a1b6c14121b0f2c4b1207c05aefb87c3417f8
Zeyneppsahin6 <zeynepsahin022@gmail.com>
2024-12-11 03:55:11 +0300

    Yeni senaryolar eklenmiştir.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -21,30 +21,21 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void unSuccessfulLogin() throws InterruptedException {
-        driver.findElement(By.id("login-email")).sendKeys(mail);
-        driver.findElement(By.name("login-password")).sendKeys("Abcdef123..");
-        driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).click();
-        sleep(3000);
-        String warningMessage = driver.findElement(By.cssSelector("[class=\"message\"]")).getText();
-        Assert.assertEquals(warningMessage, "E-posta adresiniz ve/veya şifreniz hatalı.");
+        loginPage.fillMail("zeynep@icloud.com")
+                .fillPassword(password)
+                .clickLogin();
 
-        driver.findElement(By.id("login-email")).clear();
-        driver.findElement(By.id("login-email")).sendKeys("zeynepsahin@icloud.com");
-        driver.findElement(By.name("login-password")).clear();
-        driver.findElement(By.name("login-password")).sendKeys(password);
-        driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).click();
         sleep(3000);
-        String warningMessage2 = driver.findElement(By.cssSelector("[class=\"message\"]")).getText();
-        Assert.assertEquals(warningMessage2, "E-posta adresiniz ve/veya şifreniz hatalı.");
+        Assert.assertEquals(loginPage.getErrorMessage(), "E-posta adresiniz ve/veya şifreniz hatalı.");
     }
 
     @Test
     public void validateLogin() throws InterruptedException {
-        driver.findElement(By.id("login-email")).sendKeys("zeynepsahin022icloud.com");
-        driver.findElement(By.name("login-password")).sendKeys(password);
-        driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).click();
+        loginPage.fillMail("zeynepicloud.com")
+                .fillPassword(password)
+                .clickLogin();
+
         sleep(3000);
-        String warningMessage = driver.findElement(By.cssSelector("[class=\"message\"]")).getText();
-        Assert.assertEquals(warningMessage, "Lütfen geçerli bir e-posta adresi giriniz.");
+        Assert.assertEquals(loginPage.getErrorMessage(), "Lütfen geçerli bir e-posta adresi giriniz.");
     }
 }
