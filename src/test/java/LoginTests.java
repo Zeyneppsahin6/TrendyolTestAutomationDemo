4cfaf6a96144ebce44d409f22e21608fee838213
Zeyneppsahin6 <zeynepsahin022@gmail.com>
2024-12-12 03:22:40 +0300

    Add allure report

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,32 +10,47 @@ public class LoginTests extends BaseTest {
     LoginPage loginPage = new LoginPage();
     MainPage mainPage = new MainPage();
 
-    @Test
+    @Test(description = "Başarılı kullanıcı girişi kontrolü")
     public void SuccessfulLogin() throws InterruptedException {
         loginPage.fillMail(mail)
                 .fillPassword(password)
                 .clickLogin();
         sleep(3000);
-        Assert.assertEquals("Hesabım", mainPage.getUserInfo());
+        assertEqualsText("Hesabım", mainPage.getUserInfo());
     }
 
-    @Test
+    @Test(description = "Başarısız kullanıcı girişi kontrolü")
     public void unSuccessfulLogin() throws InterruptedException {
         loginPage.fillMail("zeynep@icloud.com")
                 .fillPassword(password)
                 .clickLogin();
 
         sleep(3000);
-        Assert.assertEquals(loginPage.getErrorMessage(), "E-posta adresiniz ve/veya şifreniz hatalı.");
+        assertEqualsText(loginPage.getErrorMessage(), "E-posta adresiniz ve/veya şifreniz hatalı.");
     }
 
-    @Test
+    @Test(description = "Geçersiz karakter girişi kontrolü")
     public void validateLogin() throws InterruptedException {
         loginPage.fillMail("zeynepicloud.com")
                 .fillPassword(password)
                 .clickLogin();
 
         sleep(3000);
-        Assert.assertEquals(loginPage.getErrorMessage(), "Lütfen geçerli bir e-posta adresi giriniz.");
+        assertEqualsText(loginPage.getErrorMessage(), "Lütfen geçerli bir e-posta adresi giriniz.");
+    }
+
+    @Test(description = "Max-min karakter kontrolü")
+    public void maxMinCharacterControl() throws InterruptedException {
+        loginPage.clickLogin();
+
+        sleep(3000);
+        assertEqualsText(loginPage.getErrorMessage(), "Lütfen e-posta ve/veya şifre alanını doldurunuz.");
+
+        loginPage.fillMail("ksgökdhglhugşdhgidzgjzsşrghıaegjsgnzsşghıairtjpgodfmgjldxkdfnbzlkdhfgorgıspfmzslfbnxdışheipıjfsmddmjvzdfghdzşfgoıhgkgheslgaeşgnzşgawfnlkfxdgjdşzjgzi")
+                .fillPassword("ldgjxdşogjihudiphobzdşfbjzoşdıghaşoıfjszlghseroituaweşfjdhgşaekmzdfhbzhıjiagkzRŞhjseşywğfzlihesripğıxfljhphmfjlhiğrhıdrğyıiHmfşthojesidxşhjosrthy")
+                .clickLogin();
+
+        sleep(3000);
+        assertEqualsText(loginPage.getErrorMessage(), "Lütfen geçerli bir e-posta adresi giriniz.");
     }
 }
